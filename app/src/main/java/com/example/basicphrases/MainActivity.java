79316482c90a37fec097de1e141a16d22e08e19c
swastik2703 <swastikvasistha2127@gmail.com>
2022-08-06 19:03:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/basicphrases/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.basicphrases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    public void playPhrase(View view){
+        Button buttonPressed = (Button) view;
+        Log.i("button pressed", buttonPressed.getTag().toString());
+        MediaPlayer music = MediaPlayer.create(MainActivity.this, getResources().getIdentifier(buttonPressed.getTag().toString(), "raw", getPackageName()));
+        music.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
